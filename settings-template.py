32b5688831bf8f5667d836f7846f21661277f53c
mattmorten <mattmorten@googlemail.com>
2015-01-14 17:31:33 -0500

    Tabs to spaces

--- settings-template.py
+++ settings-template.py
@@ -1,11 +1,11 @@
 SETTINGS = {
-	## Your flowthings.io username
-	"user"="<your username>",
+    ## Your flowthings.io username
+    "user"="<your username>",
 
-	## Your flowthings.io Master Token
-	"master_token"="<your master token>",
+    ## Your flowthings.io Master Token
+    "master_token"="<your master token>",
 
-	## The host and port to run this example on
-	"host"="localhost",
-	"port"=8100
+    ## The host and port to run this example on
+    "host"="localhost",
+    "port"=8100
 }
